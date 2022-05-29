0313c21a2af27eae561795b522b2e89948f9a385
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-05-29 14:46:04 +0200

    Add event listnerer for buttons and make it show text content in display div

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ const decimalButton = document.querySelector('.decimal');
 const clearAllButton = document.querySelector('.clearAll');
 const equalButton = document.querySelector('.equal');
 
+const display = document.querySelector('.display');
+
+
+// Listen for click
+
+keys.forEach(key => key.addEventListener('click', function(e){
+  let text = e.target.textContent;
+  text === 'AC' ? display.innerHTML = '' : display.innerHTML = text;
+  console.log(e.target.textContent);
+}))
