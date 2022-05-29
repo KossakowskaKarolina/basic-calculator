ae143e2a1578b47116d94b3b7d72f4106c0d381e
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-05-29 14:38:57 +0200

    Add variables to pick all buttons

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Pick all buttons
+
+const keys = document.querySelectorAll('.keys');
+const operatorButtons = document.querySelectorAll('.operator');
+const numberButtons = document.querySelectorAll('.number');
+const decimalButton = document.querySelector('.decimal');
+const clearAllButton = document.querySelector('.clearAll');
+const equalButton = document.querySelector('.equal');
+
