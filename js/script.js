a65e09a7daeae647e1191347617079e647abfc3f
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-05-30 17:27:33 +0200

    Create variables and add listeners for buttons

--- js/script.js
+++ js/script.js
@@ -10,10 +10,67 @@ const equalButton = document.querySelector('.equal');
 const display = document.querySelector('.display');
 
 
-// Listen for click
+// Define variables
 
-keys.forEach(key => key.addEventListener('click', function(e){
-  let text = e.target.textContent;
-  text === 'AC' ? display.innerHTML = '' : display.innerHTML = text;
-  console.log(e.target.textContent);
+let equation = [];
+let firstNumber = [];
+let firstNumberShow;
+let secondNumber = [];
+let secondNumberShow;
+let result;
+
+
+// Clean variables above
+
+function clearAll(){
+  equation.length = 0;
+  firstNumber.length = 0;
+  secondNumber.length = 0;
+  secondNumberShow = undefined;
+}
+
+
+// Listen for click on numbers
+
+numberButtons.forEach(number => number.addEventListener('click', function(e){
+  let text = e.target.textContent; // shows what was clicked
+
+  if(equation.length === 0){
+    firstNumber.push(text);
+    firstNumberShow = firstNumber.join('');
+    display.innerHTML = firstNumberShow;
+  } else {
+    secondNumber.push(text);
+    secondNumberShow = secondNumber.join('');
+    display.innerHTML = secondNumberShow;
+  }
 }))
+
+
+// Listen for click on operators
+
+operatorButtons.forEach(operator => operator.addEventListener('click', function(e){
+  if(firstNumberShow){
+    equation.push(firstNumberShow);
+    equation.push(e.target.textContent);
+  }
+}))
+
+// Listen for click on equal button - make actual operation
+
+equalButton.addEventListener('click', function(e){
+  equation.push(secondNumberShow);
+  result = equation.join('');
+  display.innerHTML = eval(result);
+  clearAll();
+  firstNumberShow = eval(result);
+})
+
+
+// Listen for click on allClear button - reset calculator
+
+clearAllButton.addEventListener('click', function(e){
+  clearAll();
+  firstNumberShow = undefined;
+  display.innerHTML = '0';
+})
